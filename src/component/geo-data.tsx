8aa5c8e63265af9e4f36fb6a29d340917226641b
Ariel <arielah1997@gmail.com>
2024-02-19 22:15:12 +0200

    feat: expose more data

--- src/component/geo-data.tsx
+++ src/component/geo-data.tsx
@@ -2,8 +2,11 @@ export default function GeoData({ data }: { data: any }) {
   return (
     <div>
       <h1>Weather</h1>
-      <p>Temperature: {data.main.temp}</p>
-      <p>Feels like: {data.main.feels_like}</p>
+      <h3>
+        You're in <u>{data.name}</u>, weather is {data.weather[0].description}.
+      </h3>
+      <p>Temperature: {data.main.temp}c</p>
+      <p>Feels like: {data.main.feels_like}c</p>
       <p>Humidity: {data.main.humidity}</p>
       <p>Pressure: {data.main.pressure}</p>
     </div>
